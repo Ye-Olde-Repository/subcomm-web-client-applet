48b43a03de10c777225fb38fedc4629ce6d4ce88
roylaurie <roy.laurie@gmail.com>
2012-07-08 21:38:50 -0700

    connection no longer blocks. refactored connection thread as static class. exception handling interface.

---
+++ src/main/java/com/roylaurie/subcomm/ui/web/client/applet/ConnectThread.java
@@ -0,0 +1,66 @@
+package com.roylaurie.subcomm.ui.web.client.applet;
+
+import java.io.IOException;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.roylaurie.subcomm.client.SubcommClient;
+
+public final class ConnectThread extends Thread {
+    public static final String SUBCOMM_CONNECT = "SubcommConnect";
+    private static final String EMPTY_STR = "";
+    private static final Logger LOG = Logger.getLogger(ConnectThread.class.getCanonicalName()); 
+    
+    private final SubcommUIWebClientApplet mApplet;
+    private final String mUri;
+    private final SubcommClient mClient;
+    
+    public ConnectThread(SubcommUIWebClientApplet applet, String uri, SubcommClient client) {
+        mApplet = applet;
+        mUri = uri;
+        mClient = client;
+    }
+
+    @Override
+    public void run() {
+        try {
+            LOG.info("Connecting to `" + mUri + "`.");
+            
+            AccessController.doPrivileged(
+                new PrivilegedAction<String>() {
+                    public String run() {
+                        try {
+                            mClient.connect();
+                            if (mClient.connected()) {
+                                LOG.info("Connected to `" + mUri + "`.");
+                            } else {
+                                mApplet.notifyConnectionFailed(
+                                    mUri,
+                                    mClient,
+                                    new IOException("Client not connected after connect()!")
+                                );
+                            } 
+                        } catch (Exception e) {
+                            mApplet.notifyConnectionFailed(mUri, mClient, e);
+                            mClient.disconnect();
+                        }
+
+                        return EMPTY_STR;
+                    }
+                }
+            );
+
+            mClient.joinDefaultArena();
+            mApplet.notifyConnectionComplete(mUri, mClient);
+        } catch (Exception e) {
+            mApplet.notifyConnectionFailed(mUri, mClient, e);
+            if (mClient != null) {
+                mClient.disconnect();
+            }
+
+            LOG.log(Level.SEVERE, "Failed to connect to `" + mUri + "`.", e);
+        }
+    }
+}
--- src/main/java/com/roylaurie/subcomm/ui/web/client/applet/SubcommUIWebClientApplet.java
+++ src/main/java/com/roylaurie/subcomm/ui/web/client/applet/SubcommUIWebClientApplet.java
@@ -1,91 +1,105 @@
 package com.roylaurie.subcomm.ui.web.client.applet;
 
 import java.applet.Applet;
-import java.io.IOException;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import com.roylaurie.subcomm.client.SubcommNetchatClient;
+import com.roylaurie.subcomm.client.SubcommClient;
+import com.roylaurie.subcomm.client.netchat.SubcommNetchatClient;
 
 public final class SubcommUIWebClientApplet extends Applet {
     private static final Logger LOG = Logger.getLogger(SubcommUIWebClientApplet.class.getCanonicalName());
     private static final long serialVersionUID = 1L;
-    private static final String SUBCOMM_CONNECT = "SubcommConnect";
-    private final Map<String, SubcommClient> mClientMap = new HashMap<String, SubcommClient>();
-    private final Object mClientLock = new Object();
     
-    public void connect(String id, String hostname, int port, String username, String password) {
-        synchronized(mClientLock) {
+    /* @var Map<String, SubcommClient> mClientMap Maps uri => client object. */
+    private final Map<String, SubcommClient> mClientMap = new HashMap<String, SubcommClient>();
+    /* @var Map<String, SubcommClient> mExceptionMap Maps uri => exception object. */
+    private final Map<String, Exception> mExceptionMap = new HashMap<String, Exception>();
+    
+    public String connect(String id, String hostname, int port, String username, String password) {
+        final String uri = username + '@' + hostname + ':' + port + '#' + id;
+        synchronized(mClientMap) {
             if (mClientMap.containsKey(id))
-                return;
+                return uri;
+            
+            mClientMap.put(uri, null);
         }
 
-        final String clientId = id;
-        final SubcommNetchatClient client = new SubcommNetchatClient(hostname, port, username, password);
-        final String uri = username + '@' + hostname + ':' + port + '#' + id;
-        Thread connectThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    LOG.info("Connecting to `" + uri + "`.");
-                    
-                    AccessController.doPrivileged(
-                        new PrivilegedAction<String>() {
-                            public String run() {
-                                try {
-                                    client.connect();
-                                    if (!client.connected()) {
-                                        throw new IOException("Client not connected after connect()!");
-                                    }
-                                    
-                                    LOG.info("Connected to `" + uri + "`.");
-                                } catch (IOException e) {
-                                    LOG.log(Level.SEVERE, "Failed to connect to `" + uri + "`.", e);
-                                    throw new RuntimeException(e);
-                                }
-
-                                return "";
-                            }
-                        }
-                    );
-                    
-
-                    
-                    client.joinDefaultArena();
-                    synchronized(mClientLock) {
-                        mClientMap.put(clientId, client);
-                    }
-                } catch (Exception e) {
-                    if (client != null) {
-                        client.disconnect();
-                    }
-                    
-                    LOG.log(Level.SEVERE, "Failed to connect to `" + uri + "`.", e);
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        
-        connectThread.setName(SUBCOMM_CONNECT);
+        final SubcommClient client = new SubcommNetchatClient(hostname, port, username, password);
+        Thread connectThread = new ConnectThread(this, uri, client);
         connectThread.start();
+        return uri;
     }
     
-    public SubcommNetchatClient getClient(String id) {
-        synchronized(mClientLock) {
-            return ( mClientMap.get(id) );
+    /**
+     * Retrieves the client for the specified uri, if any.
+     * @param String uri
+     * @return SubcommClient NULL if no client found
+     */
+    public SubcommClient getClient(String uri) {
+        synchronized(mClientMap) {
+            return ( mClientMap.get(uri) );
         }
+    }
+    
+    /**
+     * Retrieves the next thrown exception, if any, for the specified client.
+     * @param String uri The client URI.
+     * @return Exception NULL if no exceptions thrown
+     */
+    public Exception nextClientException(String uri) {
+        synchronized(mExceptionMap) {
+            return mExceptionMap.remove(uri);
+        }
+    }
+    
+    /* package */ void notifyConnectionComplete(String uri, SubcommClient client) {
+        synchronized(mClientMap) {
+            // prevent leaked connections by bounds checking
+            if (!mClientMap.containsKey(uri))
+                throw new IllegalArgumentException("Unknown uri `" + uri + "` for notification of completion.");
+            
+            SubcommClient queuedClient = mClientMap.get(uri);
+            if (queuedClient != null && queuedClient.connected())
+                throw new IllegalArgumentException("Connection `" + uri + "` already notified for completion.");
+            
+            mClientMap.put(uri, client);
+        }
+    }
+    
+    /* package */ void notifyConnectionFailed(String uri, SubcommClient client, Exception e) {
+        synchronized(mExceptionMap) {
+            mExceptionMap.put(uri, e);
+        }
+    }
+    
+    /**
+     * Quietly disconnects the client for the given uri.
+     * @param String uri
+     */
+    public void disconnect(String uri) {
+        final SubcommClient client;
+        synchronized(mClientMap) {            
+            synchronized(mExceptionMap) {
+                client = mClientMap.remove(uri);
+                mExceptionMap.remove(uri);
+            }
+        }
+        
+
+        if (client == null)
+            return;
+        
+        client.disconnect();
     }
     
     @Override
     public void stop() {
         LOG.info("STOP");
         super.stop();
-        synchronized(mClientLock) {
-            for (SubcommNetchatClient client : mClientMap.values()) {
+        synchronized(mClientMap) {
+            for (SubcommClient client : mClientMap.values()) {
                 final String uri = client.getUsername() + '@' + client.getUsername() + ':' + client.getUsername();
                 client.disconnect();
                 LOG.info("Disconnected client `" + uri + "`.");
